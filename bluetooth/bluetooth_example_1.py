310716009844356b08fec00d7290ed8dc2855eab
west_laker <390879565@qq.com>
2022-09-06 14:29:50 +0800

    commit

---
+++ bluetooth/bluetooth_example_1.py
@@ -0,0 +1,9 @@
+# linux platform only
+# bluetooth low energy scan
+from bluetooth.ble import DiscoveryService
+
+service = DiscoveryService()
+devices = service.discover(2)
+
+for address, name in devices.items():
+    print("name: {}, address: {}".format(name, address))
