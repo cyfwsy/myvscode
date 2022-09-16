4a50497835a26c260f509090e91712b1efffbe6b
west_laker <390879565@qq.com>
2022-09-16 14:52:12 +0800

    commit

--- hook_example/using_hook.py
+++ hook_example/using_hook.py
@@ -7,20 +7,20 @@ class LazyRecord:
         setattr(self,name,value)
         return value
     
-# data = LazyRecord()
-# print('Before',data.__dict__)
-# print('foo',data.foo)
-# print('After',data.__dict__)
+data = LazyRecord()
+print('Before',data.__dict__)
+print('foo',data.foo)
+print('After',data.__dict__)
 
-class LoggingLazyRecord(LazyRecord):
-    def __getattr__(self, name):
-        print(f'*Called __getattr__({name !r})',f'populating \
-        instance dictionary')
-        result = super().__getattr__(name)
-        print(f'*Returning {result !r} ')
-        return result
+# class LoggingLazyRecord(LazyRecord):
+#     def __getattr__(self, name):
+#         print(f'*Called __getattr__({name !r})',f'populating \
+#         instance dictionary')
+#         result = super().__getattr__(name)
+#         print(f'*Returning {result !r} ')
+#         return result
     
-data = LoggingLazyRecord()
-print('exists >>>',data.exists)
-print('first foo >>>',data.foo)
-print('second foo >>>',data.foo)
+# data = LoggingLazyRecord()
+# print('exists >>>',data.exists)
+# print('first foo >>>',data.foo)
+# print('second foo >>>',data.foo)
---
+++ hook_example/using_hook_getattribute_example1.py
@@ -0,0 +1,30 @@
+# 验证 __getattribute__() 特性
+class CustomAttribute:
+    def __init__(self,custom_dict={}):
+        self.dict_1 = custom_dict
+        self.exists = 5
+        
+    def __getattribute__(self, name: str):
+        print(f'* Called __getattribute__({name !r})')
+        try:
+            value = super().__getattribute__(name)
+            print(f'Found {name !r} ,returning {value !r}')
+            if name != 'dict_1':
+                self.dict_1[name] = value
+            return value
+        except AttributeError:
+            print('no such attribute')
+            self.dict_1[name] = 'Value'
+            
+            
+        
+data = CustomAttribute()
+print(data.__dict__)
+print(data.dict_1)
+print(data.exists)
+# print(data.foo)
+data.foo = 'value of foo'
+print(data.foo)
+print(data.dict_1)
+# print(data.__dict__)
+            
